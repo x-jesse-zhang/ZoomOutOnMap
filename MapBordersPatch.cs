dd01eef2899646a708e07d868202af37130fbfe9
Jesse Zhang <macisasandwich@gmail.com>
2022-11-24 19:31:38 -0800

    Add project files.

---
+++ MapBordersPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using SandBox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZoomOutOnMap
+{
+    [HarmonyPatch]
+    internal class MapBordersPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MapScene), "GetMapBorders")]
+        public static void GetMapBorders(out float maximumHeight)
+        {
+            maximumHeight = 1000f;
+        }
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,27 @@
+﻿using TaleWorlds.MountAndBlade;
+using HarmonyLib;
+
+namespace ZoomOutOnMap
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            Harmony h = new Harmony("ZoomOutOnMap");
+            h.PatchAll();
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
